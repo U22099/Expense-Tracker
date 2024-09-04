6180886ecb68bd45c702d8502b381c963748c2f9
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-04 20:31:52 +0100

    ---

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 
-export const { handler: {GET, POST}, auth, signIn, signOut } = NextAUth({
+export const { handler: {GET, POST}, auth, signIn, signOut } = NextAuth({
     providers: [
         GitHub({
             clientId: process.env.GITHUB_ID,
