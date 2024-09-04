f99ac1074d0419c5ec1d08b4cd8db352cce8df43
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-04 20:36:03 +0100

    ---

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -7,8 +7,8 @@ const page = async () => {
     <div>
         <div className="flex text-black bg-white text-3xl font-serif">Homepage</div>
         <img src={session.user.image} className="object-cover rounded"/>
-        <div className="flex text-black bg-white text-3xl font-serif">session.user.name</div>
-        <div className="flex text-black bg-white text-3xl font-serif">session.user.email</div>
+        <div className="flex text-black bg-white text-3xl font-serif">{session.user.name}</div>
+        <div className="flex text-black bg-white text-3xl font-serif">{session.user.email}</div>
     </div>
   )
 }
