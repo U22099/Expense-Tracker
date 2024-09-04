9791ecd0e991ecb05bb758601d1baa7ac78da8bf
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-04 20:09:35 +0100

    ---

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { handleGitHubLogin } from "@/lib/action";
+import {FaGithub, FaGoogle} from "react-icons/fa6";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center gap-3 w-screen h-screen bg-white dark:bg-black">
       <h1>Expense Tracker</h1>
       <Link href="/homepage">Homepage</Link>
+      <form action={}>
+        <button className="flex w-3/4 md:w-2/4 rounded-full bg-black dark:bg-white dark:text-black"><FaGoogle className="inline fill-white dark:fill-black"/> SignIn with Google</button>
+      </form>
       <form action={handleGitHubLogin}>
-        <button>SignIn with GitHub</button>
+        <button className="flex w-3/4 md:w-2/4 rounded-full bg-black dark:bg-white dark:text-black"><FaGithub className="inline fill-white dark:fill-black"/> SignIn with GitHub</button>
       </form>
     </div>
   );
