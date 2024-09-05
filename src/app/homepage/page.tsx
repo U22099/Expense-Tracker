1eec5f357b7c9e5f07ead259e24dbd196d808be7
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-05 05:36:15 +0100

    ---

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -9,7 +9,7 @@ const page = async () => {
   return (
     <div>
         <div className="flex text-black bg-white text-3xl font-serif">Homepage</div>
-        <Image src={session.user?.image ?? ''} className="object-cover rounded"/>
+        <Image src={session.user?.image ?? ''} alt="image" width={200} height={200} className="object-cover rounded"/>
         <div className="flex text-black bg-white text-3xl font-serif">{session.user?.name}</div>
         <div className="flex text-black bg-white text-3xl font-serif">{session.user?.email}</div>
         <form action={handleSocialLogout}>
