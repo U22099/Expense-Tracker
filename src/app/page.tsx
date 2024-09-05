2e16c4f5d45b83efcdd6e2718b860b97f8b58008
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-05 14:57:55 +0100

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,8 @@ import { handleSocialLogin } from "@/lib/action";
 import { FaGithub } from "react-icons/fa6";
 import { FcGoogle  } from "react-icons/fc";
 import Image from "next/image";
-import { useState } from "react";
 
 export default function Home() {
-  const [count, setCount] = useState(1);
   return (
     <div className="flex flex-col justify-start items-center gap-3 w-screen h-screen bg-white dark:bg-black p-3">
       <Image src="logo7.png" alt="logo" className="rounded-full object-cover w-28 h-28 md:w-32 md:h-32 mx-auto mb-4 mt-4"/>
@@ -17,7 +15,7 @@ export default function Home() {
         <button name="provider" value="google" className="flex p-4 px-5 items-center w-[80vw] md:w-[40vw] rounded-full bg-black text-white dark:bg-white dark:text-black text-[1.2em] md:text-[1.5em] flex justify-center gap-4"><FcGoogle className="text-3xl"/> Sign In with Google</button>
         <button name="provider" value="github" className="flex p-4 px-5 items-center w-[80vw] md:w-[40vw] rounded-full bg-black text-white dark:bg-white dark:text-black text-[1.3em] md:text-[1.5em] flex justify-center gap-4"><FaGithub className="fill-[#333333] text-3xl"/> Sign In with GitHub</button>
       </form>
-      <hr className="w-[75vw] md:w-[38vw] mx-auto border-black dark:border-white">
+      <hr className="w-[75vw] md:w-[38vw] mx-auto border-black dark:border-white"/>
     </div>
   );
 }
