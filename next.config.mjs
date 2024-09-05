6a69641c511817eaad1b1e64f22be3d30c5f920a
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-05 05:13:11 +0100

    ---

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: {
+        remotePatterns: [
             {
                 protocol: "https",
-                hostname: "avatars.githubusercontent.com"
-            }
-        }
-    }
+                hostname: "avatars.githubusercontent.com",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
