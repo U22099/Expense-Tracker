040c8de6ff9ce37ceac3ce85da3b18a06e971339
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-06 23:42:57 +0100

    ---

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,27 +1,30 @@
 
+type objType = {
+    [index: string]: string,
+}
 const authConfig = {
     providers: [],
     callbacks: {
-        async jwt({token, user}){
+        async jwt({token, user}: any){
             if(token){
                 console.log(user);
                 if(user.id) token.id = user.id;
             }
             return token
         },
-        async session({session, token}){
+        async session({session, token}: any){
             if(token){
                 if(token.id) session.id = token.id
             }
             return session
         },
-        authorized({auth, req}){
+        authorized({auth, req}: any){
             const user = auth?.user;
             const isOnHomepage = req.nextUrl?.pathname.startsWith("/homepage");
 
             if(!user&&isOnHomepage) return false
 
-            if(user&&!isOnHomepage) return Response.redirect(new URL("/", req.nextUrl));
+            if(user&&!isOnHomepage) return Response.redirect(new URL("/", req?.nextUrl));
 
             return true
         }
