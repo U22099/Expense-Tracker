afce1df8eeac3f3a591adeecbf19aaa4e6639651
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-06 15:44:55 +0100

    Add files via upload

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -5,7 +5,7 @@ import { useFormState } from "react-dom";
 import Link from "next/link";
 
 export default function Page() {
-  const [state, formAction] = useFormState(handleRegister , undefined)
+  const [state, formAction] = useFormState(handleRegister , undefined) as [{error?: string, success?: string}, typeof formAction]
   return (
     <div className="flex flex-col justify-center items-center gap-3 w-screen min-h-screen bg-gray-200 dark:bg-black p-3">
       <h1 className="w-[70vw] md:w-[30vw] text-[2em] md:text-[3em] text-bold text-black dark:text-white text-center text-wrap custom-font">Sign Up for Expense Tracker</h1>
@@ -13,7 +13,7 @@ export default function Page() {
         <input type="username" name="username" placeholder="Username" className="flex p-2 items-center w-[80vw] md:w-[40vw] rounded-lg bg-white text-[1.2em] md:text-[1.5em] shadow-md text-black custom-font"/>
         <input type="email" name="email" placeholder="Email" className="flex p-2 items-center w-[80vw] md:w-[40vw] rounded-lg bg-white text-[1.2em] md:text-[1.5em] shadow-md text-black custom-font"/>
         <input type="password" name="password" placeholder="Password" className="flex p-2 items-center w-[80vw] md:w-[40vw] rounded-lg bg-white text-[1.2em] md:text-[1.5em] shadow-md text-black custom-font"/>
-        {state?.error&&<p className="text-red-600 font-mono">{state?.error}</p>}
+        {state?.error && <p className="text-red-600 font-mono">{state.error}</p>}
         <button className="p-2 px-5 items-center w-[80vw] md:w-[40vw] rounded-full bg-black text-white dark:bg-white dark:text-black text-[1.3em] md:text-[1.5em] flex justify-center gap- custom-font"> Sign Up </button>
       </form>
       <p className="text-gray-700">Already have an account? <Link href="/" className="text-black custom-font">Sign In</Link></p>
