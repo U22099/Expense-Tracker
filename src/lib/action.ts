9730b20ba11e5b14f8b3b870a533fc096cf1feee
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-06 16:11:54 +0100

    Add files via upload

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -7,17 +7,17 @@ import { hash } from "bcryptjs";
 export const handleSocialLogin = async (formData: FormData) => {
     const provider = formData.get("action") as string;
 
-    await signIn(provider, {redirectTo: "/homepage"});
+    await signIn(provider, {callbackUrl: "/homepage"});
 }
 
 export const handleLogout = async () => {
-    await signOut({redirectTo: "/"});
+    await signOut({callbackUrl: "/"});
 }
 
 export const handleLogin = async (formData: FormData) => {
     const { email, password } = Object.fromEntries(formData);
 
-    await signIn("credentials", { email, password, redirectTo: "/homepage" });
+    await signIn("credentials", { email, password, callbackUrl: "/homepage" });
 }
 
 export const handleRegister = async (prevState, formData: FormData): Promise<Object> => {
@@ -38,7 +38,7 @@ export const handleRegister = async (prevState, formData: FormData): Promise<Obj
             image: "/avatar.JPG",
         })
         await newUser.save();
-        await signIn("credentials", { email, password, redirectTo: "/homepage" });
+        await signIn("credentials", { email, password, callbackUrl: "/homepage" });
         return {success: true}
     } catch(err) {
         return {error: `Error while logging user in: ${err}`}
