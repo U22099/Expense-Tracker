84d1af0848573627d1c993f213d7a1b027a287a7
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-07 10:47:34 +0100

    ---

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,24 @@
 import { NextResponse, type NextRequest} from "next/server";
-import { auth } from "@/lib/auth";
+import NextAuth from "next-auth";
+import { authConfig } from "@/lib/auth.config";
 
-export default auth((req: any) => {
-    if(!req.auth && req.nextUrl?.pathname !== "/"){
-        return Response.redirect(new URL("/", req.nextUrl?.origin));
-    }
-})
-// import NextAuth from "next-auth";
-// import { authConfig } from "@/lib/auth.config";
+const { auth } = NextAuth(authConfig);
 
-// const { auth } = NextAuth(authConfig);
+export default async function middleware(request: NextRequest){
+    const session = await auth();
+    console.log(session, "middleware");
+    const { nextUrl } = request;
+    const authenticated: boolean = !!session?.user;
+    const currentPath: string = nextUrl.pathname;
+    const publicRoutes: string[] = ['/', '/register', '/api/auth/callback/google', '/api/auth/callback/github']
+    const isPublicRoute: boolean = (publicRoutes.find(route => currentPath.startsWith(route))) ? true : false;
+    console.log(isPublicRoute);
+    const homepage: string = "/homepage";
 
-// export default async function middleware(request: NextRequest){
-//     const session = await auth();
-//     console.log(session, "middleware");
-//     const { nextUrl } = request;
-//     const authenticated: boolean = !!session?.user;
-//     const currentPath: string = nextUrl.pathname;
-//     const publicRoutes: string[] = ['/', '/register', '/api/auth/callback/google', '/api/auth/callback/github']
-//     const isPublicRoute: boolean = (publicRoutes.find(route => currentPath.startsWith(route))) ? true : false;
-//     const homepage: string = "/homepage";
+    if(isPublicRoute && authenticated) return NextResponse.redirect(new URL(homepage, "https://expense-tracker-z9y9.onrender.com"));    
+    if(!isPublicRoute && !authenticated) return NextResponse.redirect(new URL(publicRoutes[0], "https://expense-tracker-z9y9.onrender.com"));
 
-//     console.log(nextUrl, request, isPublicRoute, authenticated);
-//     //if(isPublicRoute && authenticated) return NextResponse.redirect(new URL(homepage, nextUrl));    
-//     //if(!isPublicRoute && !authenticated) return NextResponse.redirect(new URL(publicRoutes[0], nextUrl));
-
-// }
+}
 
 
 export const config = {
