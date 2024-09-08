17f1c6aab3a096cf100fedf2b4814354af09e58d
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-08 23:09:32 +0100

    ---

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,10 +33,10 @@ export default function Page(){
       <Image src="/Logo.JPG" alt="logo" className="rounded-full object-cover w-32 h-32 md:w-40 md:h-40 mx-auto mb-2 mt-4" />
       <h1 className="text-[2.3em] md:text-[4em] text-bold text-black dark:text-white text-center custom-font">Expense Tracker</h1>
       <p className="text-black dark:text-white text-center custom-font" >Track, manage, and master your expenses with ease</p>
-      <motion.div variants={parentVariant}>
+      <motion.div variants={parentVariant} initial="hidden" animate="visible">
         <form action={handleSocialLogin} className="flex flex-col gap-3 mx-auto mt-4">
-          <motion.button variants={childrenVariant} name="provider" value="google" className="p-2 px-5 items-center w-[80vw] md:w-[40vw] rounded-full bg-black text-white dark:bg-white dark:text-black text-[1.2em] md:text-[1.5em] flex justify-center gap-4"><FcGoogle className="text-3xl custom-font"/> Sign In with Google</motion.button>
-          <motion.button variants={childrenVariant} name="provider" value="github" className="p-2 px-5 items-center w-[80vw] md:w-[40vw] rounded-full bg-black text-white dark:bg-white dark:text-black text-[1.3em] md:text-[1.5em] flex justify-center gap-4"><FaGithub className="fill-[#333333] text-3xl custom-font"/> Sign In with GitHub</motion.button>
+          <motion.button variants={childrenVariant} initial="hidden" animate="visible" name="provider" value="google" className="p-2 px-5 items-center w-[80vw] md:w-[40vw] rounded-full bg-black text-white dark:bg-white dark:text-black text-[1.2em] md:text-[1.5em] flex justify-center gap-4"><FcGoogle className="text-3xl custom-font"/> Sign In with Google</motion.button>
+          <motion.button variants={childrenVariant} initial="hidden" animate="visible" name="provider" value="github" className="p-2 px-5 items-center w-[80vw] md:w-[40vw] rounded-full bg-black text-white dark:bg-white dark:text-black text-[1.3em] md:text-[1.5em] flex justify-center gap-4"><FaGithub className="fill-[#333333] text-3xl custom-font"/> Sign In with GitHub</motion.button>
         </form>
       </motion.div>
       <div className="flex justify-center items-center w-[65vw] md:w-[28vw] mx-auto mx-4 gap-1">
@@ -44,10 +44,10 @@ export default function Page(){
           Or
         <span className="max-w-[45%] text-black dark:text-white">-------------</span>
       </div>
-      <motion.div variants={parentVariant}>
+      <motion.div variants={parentVariant} initial="hidden" animate="visible">
         <form action={formAction} className="flex flex-col gap-3 mx-auto mt-3">
-          <motion.input variants={childrenVariant} type="email" name="email" placeholder="Email" className="flex p-2 items-center w-[80vw] md:w-[40vw] rounded-lg bg-white text-[1.2em] md:text-[1.5em] shadow-md text-black custom-font"/>
-          <motion.input variants={childrenVariant} type="password" name="password" placeholder="Password" className="flex p-2 items-center w-[80vw] md:w-[40vw] rounded-lg bg-white text-[1.2em] md:text-[1.5em] shadow-md text-black custom-font"/>
+          <motion.input variants={childrenVariant} initial="hidden" animate="visible" type="email" name="email" placeholder="Email" className="flex p-2 items-center w-[80vw] md:w-[40vw] rounded-lg bg-white text-[1.2em] md:text-[1.5em] shadow-md text-black custom-font"/>
+          <motion.input variants={childrenVariant} initial="hidden" animate="visible" type="password" name="password" placeholder="Password" className="flex p-2 items-center w-[80vw] md:w-[40vw] rounded-lg bg-white text-[1.2em] md:text-[1.5em] shadow-md text-black custom-font"/>
           {state?.error && <p className="text-red-600 font-mono text-sm">{state.error}</p>}
           <Button text="Sign In"/>
         </form>
@@ -65,6 +65,6 @@ function Button({text}: {text: string}){
   }
   const {pending}  = useFormStatus();
   return(
-    <motion.button variants={childrenVariant}  className="p-2 px-5 items-center w-[80vw] md:w-[40vw] rounded-full bg-black text-white dark:bg-white dark:text-black text-[1.3em] md:text-[1.5em] flex justify-center gap- custom-font">{pending ? <AiOutlineLoading className="animate-spin fill-white text-md dark:fill-black"/> : text} </motion.button>
+    <motion.button variants={childrenVariant} initial="hidden" animate="visible" className="p-2 px-5 items-center w-[80vw] md:w-[40vw] rounded-full bg-black text-white dark:bg-white dark:text-black text-[1.3em] md:text-[1.5em] flex justify-center gap- custom-font">{pending ? <AiOutlineLoading className="animate-spin fill-white text-md dark:fill-black"/> : text} </motion.button>
   )
 }
