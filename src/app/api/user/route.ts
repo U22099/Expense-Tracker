6ac1ab8696d6b687f2db820dbf26617a81f317ee
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-13 15:56:34 +0100

    ---

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,16 +3,16 @@ import { NextResponse } from "next/server";
 
 export const GET = () => {
     const session = cookies().get("session")?.value;
-    if(!session) return new NextResponse("", {status: 404});
+    if(!session) return NextResponse("", {status: 404});
     let user: UserObj | null = null;
     try {
         const decodedCookie = decodeURIComponent(session);
         user = JSON.parse(decodedCookie) || null;
     } catch (error) {
         console.error("Error parsing session cookie:", error);
-        return new NextResponse("", {status: 500});
+        return NextResponse("", {status: 500});
     }
-    return new NextResponse.json(user, {status: 200});
+    return NextResponse.json(user, {status: 200});
 }
 interface UserObj {
     id: string;
