972e3292359c0ed9d0d1644b36ee534fb24dc5bc
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-13 16:23:04 +0100

    ---

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -23,7 +23,7 @@ export async function fetchUser(setUser: any){
     try{
         const response = await axios.get('api/user');
         const data = response.data;
-        console.log(data);
+        console.log(response);
         if(response.status === 200) setUser(data.user);
     } catch(e){
         console.log(e);
