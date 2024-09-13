8da0147075db9dbac746125aac6c58021deefe23
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-13 09:00:17 +0100

    ---

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,11 +1,26 @@
-"use client";
-import Navbar from "@/components/navbar";
+"use server";
+import { handleLogout, getSession } from "@/lib/action";
+import { redirect } from "next/navigation";
+import Image from "next/image";
 
 
-export default function Page(){
+export default async function Navbar(){
+  const user = getSession() as {
+    id: string,
+    name: string,
+    image: string,
+    email: string
+  } | null;
+  if(!user) redirect("/");
   return (
     <div>
-        <Navbar/>
+        <div className="flex text-black bg-white text-3xl font-serif">Homepage</div>
+        <Image src={user?.image ?? ''} alt="image" className="object-cover rounded-full mx-auto w-40 h-40"/>
+        <div className="flex text-black bg-white text-3xl font-serif">{user?.name}</div>
+        <div className="flex text-black bg-white text-3xl font-serif">{user?.email}</div>
+        <form action={handleLogout}>
+          <button className="flex p-4 px-5 items-center w-[80vw] md:w-[40vw] rounded-full bg-black text-white dark:bg-white dark:text-black">Logout</button>
+        </form>
     </div>
   )
 }
