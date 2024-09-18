6b1cb27fe18d605137e0add753d3e9105baa8a6a
Daniel <156960927+U22099@users.noreply.github.com>
2024-09-19 00:05:05 +0100

    ---

--- src/components/utils/Card.tsx
+++ src/components/utils/Card.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-export default function Card(
-  children: React.ReactNode;
-) {
+export default function Card({
+  children
+}: {
+  children: React.ReactNode
+}) {
   return (
     <main className="flex flex-col justify-center bg-white dark:bg-black shadow-lg dark:border-3 dark:border-white">
         {children}
