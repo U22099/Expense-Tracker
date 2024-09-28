a088b9b4357b2136fcf55cb6592679655fa37923
U22099 <nifemiolaniyi4@gmail.com>
2024-09-28 14:27:00 +0100

    update Chart.tsx

--- src/components/bodyComponents/homeComponents/Chart.tsx
+++ src/components/bodyComponents/homeComponents/Chart.tsx
@@ -9,7 +9,7 @@ export default function Chart(){
   const categoriesColors = useData(state => state.categoriesColors);
       
     return(
-        <div className="pb-10 pt-4 h-fit flex w-[90vw] md:w-full overflow-hidden overflow-x-scroll md:overflow-x-hidden gap-4">
+        <div className="pb-10 pt-4 h-fit flex w-[90vw] md:w-full overflow-hidden overflow-x-scroll gap-4">
             <Card className="dark:border-slate-300 dark:border-2 p-2 rounded-md min-w-[90vw] md:min-w-1/2 h-80">
                 <Piechart data={data} colors={categoriesColors} value="amount"/>
             </Card>
