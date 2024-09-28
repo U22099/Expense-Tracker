89016d478808c7fb6667a28b19e0dd1808422436
U22099 <nifemiolaniyi4@gmail.com>
2024-09-28 09:29:13 +0100

    update body.tsx

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -9,7 +9,7 @@ import { useNav } from "@/store";
 export default function Body(){
   const nav = useNav(state => state.nav);
   return(
-      <div className="flex w-screen p-4 md:p-6 overflow-hidden overflow-y-scroll">
+      <div className="flex flex-col justify-start w-screen p-4 md:p-6">
           {nav === 0 && <Home />}
           {nav === 1 && <Dashboard />}
           {nav === 2 && <Settings />}
