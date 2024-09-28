361a876b6d513dd923dfd5429fe68e82d33e643a
U22099 <nifemiolaniyi4@gmail.com>
2024-09-28 06:54:37 +0100

    update

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -6,7 +6,7 @@ import NavBar from "@/components/navbar";
 
 export default async function Page(){
   return (
-    <div className="flex flex-col justify-start h-screen overflow-hidden my-6">
+    <div className="flex flex-col justify-start h-screen my-6">
       <Header/>
       <Body />
       <nav className="fixed bottom-2 w-full">
