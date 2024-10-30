dd8600b8381f5bb8a451b13d0f68fa194280370d
U22099 <nifemiolaniyi4@gmail.com>
2024-10-30 17:47:11 +0100

    update Lists.tsx

---
+++ src/components/utils/Lists.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Image from "next/image";
+import Card from "@/components/utils/Card";
+
+export default function Lists({ name, price }) {
+  return (
+
+    <div className="w-full">
+  <Card className="w-full items-center justify-between">
+    <div className="flex items-center gap-3 w-full justify-start">
+      <h1 className="font-bold text-black dark:text-white text-[1.3em] md:text-[1.5em] w-full">{name}</h1>
+    </div>
+    <p className="font-bold text-black dark:text-white text-[1em] md:text-[1.2em]">{price}
+    </p>
+  </Card>
+</div>
+  );
+}
