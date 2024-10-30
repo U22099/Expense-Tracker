2eb010ca2656f4852ee9f3bc0f6f4127fd5af7d0
U22099 <nifemiolaniyi4@gmail.com>
2024-10-30 18:47:35 +0100

    update

--- src/components/utils/Lists.tsx
+++ src/components/utils/Lists.tsx
@@ -3,7 +3,10 @@
 import Image from "next/image";
 import Card from "@/components/utils/Card";
 
-export default function Lists({ name, price }) {
+export default function Lists({ name, price } : {
+  name: string | number,
+  price: string | number,
+}) {
   return (
 
     <div className="w-full">
@@ -11,7 +14,7 @@ export default function Lists({ name, price }) {
     <div className="flex items-center gap-3 w-full justify-start">
       <h1 className="font-bold text-black dark:text-white text-[1.3em] md:text-[1.5em] w-full">{name}</h1>
     </div>
-    <p className="font-bold text-black dark:text-white text-[1em] md:text-[1.2em]">{price}
+    <p className="font-bold text-black dark:text-white text-[1em] md:text-[1.2em]">${price}
     </p>
   </Card>
 </div>
