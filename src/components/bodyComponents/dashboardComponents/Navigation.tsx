a5dfd865a2e754ab993173058d6841ce66ea9894
U22099 <nifemiolaniyi4@gmail.com>
2024-10-30 21:14:32 +0100

    update Navigation.tsx

--- src/components/bodyComponents/dashboardComponents/Navigation.tsx
+++ src/components/bodyComponents/dashboardComponents/Navigation.tsx
@@ -7,13 +7,13 @@ export default function Navigation({ setNav, nav }: {setNav: (arg: number) => vo
   return (
     <div className="w-fit flex gap-2 self-start">
       <Card
-      className={"rounded-xl bg-white dark:bg-black dark:text-white " + (nav === 0 ? " bg-black dark:bg-white dark:text-black text-white":"")}
+      className={"rounded-x " + (nav === 0 ? "bg-black dark:bg-white dark:text-black text-white":"bg-white dark:bg-black dark:text-white")}
       onClick={ () => setNav(0)} >Days  </Card>
       <Card
-      className={"rounded-xl bg-white dark:bg-black dark:text-white " + (nav === 1 ? " bg-black dark:bg-white dark:text-black text-white":"")}
+      className={"rounded-x " + (nav === 1 ? "bg-black dark:bg-white dark:text-black text-white":"bg-white dark:bg-black dark:text-white")}
       onClick={ () => setNav(1)} >Weeks</Card>
       <Card
-      className={"rounded-xl bg-white dark:bg-black dark:text-white " + (nav === 2 ? " bg-black dark:bg-white dark:text-black text-white":"")}
+      className={"rounded-x " + (nav === 2 ? "bg-black dark:bg-white dark:text-black text-white":"bg-white dark:bg-black dark:text-white")}
       onClick={ () => setNav(2)} >Months</Card>
     </div>
   )
