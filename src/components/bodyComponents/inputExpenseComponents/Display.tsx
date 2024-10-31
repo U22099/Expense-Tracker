b1a7ded3c95b587274b3d48709a47928de821d01
U22099 <nifemiolaniyi4@gmail.com>
2024-10-31 16:57:01 +0100

    update Display.tsx

--- src/components/bodyComponents/inputExpenseComponents/Display.tsx
+++ src/components/bodyComponents/inputExpenseComponents/Display.tsx
@@ -6,7 +6,7 @@ export default function Display({ value }: {value: number}) {
   const currencySymbol: string = useData(state => state.currencySymbol);
   
   return (
-    <h1 className="w-full break-words font-bold text-3xl text-center px-1 text-white dark:text-black my-3">
+    <h1 className="w-full break-words font-bold text-3xl text-center px-1 text-white dark:text-black my-3 select-none">
       {`${currencySymbol}${value.toLocaleString()}`}
     </h1>
   )
