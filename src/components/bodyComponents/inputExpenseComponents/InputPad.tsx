215e0e73de3dc78e6d1efc7a98add9c73fbab1b7
U22099 <nifemiolaniyi4@gmail.com>
2024-10-31 15:10:04 +0100

    update InputPad.tsx

--- src/components/bodyComponents/inputExpenseComponents/InputPad.tsx
+++ src/components/bodyComponents/inputExpenseComponents/InputPad.tsx
@@ -12,13 +12,13 @@ export default function InputPad({ setValue }: {setValue: (arg: number | ((prevV
         <div className="grid grid-cols-3 gap-2">
           {numberArr.map(num => <Keypad num={num} key={num} setValue={setValue}/>)}
         </div>
-        <div className="flex justify-center items-center font-lg rounded-lg p-2 bg-black dark:bg-white font-bold w-full text-center" onClick={() => setValue((prevValue: number) => parseInt(`${value}${0}`))}>
+        <div className="flex justify-center items-center font-lg rounded-lg p-2 bg-black dark:bg-white font-bold w-full text-center" onClick={() => setValue((prevValue: number) => parseInt(`${prevValue}${0}`))}>
           <p>0</p>
         </div>
       </section>
       <section>
         <div className="flex justify-center items-center font-lg rounded-lg p-2 bg-black dark:bg-white font-bold w-full text-center" onClick={() => setValue((prevValue: number) => parseInt(`${
-            value.toString().slice(0, -1)
+            prevValue.toString().slice(0, -1)
           }`))}>
           <p>delete</p>
         </div>
@@ -32,7 +32,7 @@ export default function InputPad({ setValue }: {setValue: (arg: number | ((prevV
 
 const Keypad = ({num, key, setValue}: {num: number, key: number, setValue: (arg: number) => void}) => {
   return(
-    <div key={key} className="flex justify-center items-center font-lg rounded-lg p-2 bg-black dark:bg-white font-bold" onClick={() => setValue((prevValue: number) => parseInt(`${value}${num}`))}>
+    <div key={key} className="flex justify-center items-center font-lg rounded-lg p-2 bg-black dark:bg-white font-bold" onClick={() => setValue((prevValue: number) => parseInt(`${prevValue}${num}`))}>
       <p>{num}</p>
     </div>
   )
