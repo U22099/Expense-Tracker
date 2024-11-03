9f9313436a3931d275397393da2286f57af24050
U22099 <nifemiolaniyi4@gmail.com>
2024-11-03 02:16:16 +0100

    update Home.tsx

--- src/components/bodyComponents/Home.tsx
+++ src/components/bodyComponents/Home.tsx
@@ -37,11 +37,11 @@ export default function Home(){
 }
 function getWeekAmount(expense: datatype3, date: string){
   const weekArr = (expense.filter(entry => getWeekNumber(entry.date) === getWeekNumber(date))).map(a => a.amount);
-  const weekAmount = (weekArr.reduce((a, b) => a + b, 0 ))[0] || 0;
+  const weekAmount = (weekArr.reduce((a, b) => a + b, 0 ));
   return weekAmount;
 }
 function getMonthAmount(expense: datatype3, date: string){
   const monthArr = (expense.filter(entry => entry.date.slice(3) === date.slice(3))).map(a => a.amount);
-  const monthAmount = (monthArr.reduce((a,b) => a + b, 0))[0] || 0;
+  const monthAmount = (monthArr.reduce((a,b) => a + b, 0));
   return monthAmount;
 }
